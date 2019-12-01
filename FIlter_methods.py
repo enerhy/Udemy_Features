88106d1b2ec7fce0212128e31b759ec923a3777d
Spasov <51121651+enerhy@users.noreply.github.com>
2019-12-01 18:56:02 +0200

    Update FIlter_methods.py

--- FIlter_methods.py
+++ FIlter_methods.py
@@ -73,28 +73,108 @@ for i in range(0, len(X_train.columns)):
 set(duplicated_feat) 
 len(set(duplicated_feat))
 
-#################
-# we can go ahead and try to identify which set of features
-# are identical
+# Dropping the dupplicated features
+X_train.drop(labels=duplicated_feat, axis=1, inplace=True)
+X_test.drop(labels=duplicated_feat, axis=1, inplace=True)
 
-duplicated_feat = []
-for i in range(0, len(X_train.columns)):
 
-    col_1 = X_train.columns[i]
+--------Filtering based on Correlation
+#Visualising correlated featres
+corrmat = X_train.corr()
+fig, ax = plt.subplots()
+fig.set_size_inches(11,11)
+sns.heatmap(corrmat)
 
-    for col_2 in X_train.columns[i + 1:]:
+----Brute Force
+# with the following function we can select highly correlated features
+# it will remove the first feature that is correlated with anything else
+# without any other insight.
 
-        # if the features are duplicated
-        if X_train[col_1].equals(X_train[col_2]):
+def correlation(dataset, threshold):
+    col_corr = set()  # Set of all the names of correlated columns
+    corr_matrix = dataset.corr()
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i):
+            if abs(corr_matrix.iloc[i, j]) > threshold: # we are interested in absolute coeff value
+                colname = corr_matrix.columns[i]  # getting the name of column
+                col_corr.add(colname)
+    return col_corr
 
-            #print them
-            print(col_1)
-            print(col_2)
-            print()
+X_train.drop(labels=corr_features, axis=1, inplace=True)
+X_test.drop(labels=corr_features, axis=1, inplace=True)
+X_train.shape, X_test.shape
 
-            # and then append the duplicated one to a
-            # list
-            duplicated_feat.append(col_2)
+
+------Second Approach
+# build a dataframe with the correlation between features
+# remember that the absolute value of the correlation
+# coefficient is important and not the sign
+
+corrmat = X_train.corr()
+corrmat = corrmat.abs().unstack() # absolute value of corr coef
+corrmat = corrmat.sort_values(ascending=False)
+corrmat = corrmat[corrmat >= 0.8]
+corrmat = corrmat[corrmat < 1]
+corrmat = pd.DataFrame(corrmat).reset_index()
+corrmat.columns = ['feature1', 'feature2', 'corr']
+corrmat.head()
+
+
+# find groups of correlated features
+
+grouped_feature_ls = []
+correlated_groups = []
+
+for feature in corrmat.feature1.unique():
+    if feature not in grouped_feature_ls:
+
+        # find all features correlated to a single feature
+        correlated_block = corrmat[corrmat.feature1 == feature]
+        grouped_feature_ls = grouped_feature_ls + list(
+            correlated_block.feature2.unique()) + [feature]
+
+        # append the block of features to the list
+        correlated_groups.append(correlated_block)
+
+print('found {} correlated groups'.format(len(correlated_groups)))
+print('out of {} total features'.format(X_train.shape[1]))
+
+
+# now we can visualise each group. We see that some groups contain
+# only 2 correlated features, some other groups present several features 
+# that are correlated among themselves.
+
+for group in correlated_groups:
+    print(group)
+    print()
+
+# we can now investigate further features within one group.
+# let's for example select group 3
+
+group = correlated_groups[2]
+group
+
+# we could select the features with less missing data
+for feature in list(group.feature2.unique())+['v17']:
+    print(X_train[feature].isnull().sum())
+    
+#Alternatively, we could build a machine learning algorithm using all the
+#features from the above list, and select the more predictive one.    
+from sklearn.ensemble import RandomForestClassifier
+features = list(group.feature2.unique())+['v17']
+rf = RandomForestClassifier(n_estimators=200, random_state=39, max_depth=4)
+rf.fit(X_train[features].fillna(0), y_train)
+
+# we get the feature importance attributed by the random forest model
+importance = pd.concat(
+    [pd.Series(features),
+     pd.Series(rf.feature_importances_)], axis=1)
+
+importance.columns = ['feature', 'importance']
+importance.sort_values(by='importance', ascending=False)
+
+
+
 
 
 
